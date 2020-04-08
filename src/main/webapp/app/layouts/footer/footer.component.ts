c6696d4d80700e728adc8d14f058f493da4379fb
Marco <mdigennar@gmail.com>
2020-04-08 18:41:29 +0200

    Customize Home Page. close #16.

--- src/main/webapp/app/layouts/footer/footer.component.ts
+++ src/main/webapp/app/layouts/footer/footer.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'jhi-footer',
-  templateUrl: './footer.component.html'
+  templateUrl: './footer.component.html',
+  styleUrls: ['footer.scss']
 })
-export class FooterComponent {}
+export class FooterComponent {
+  currentYear: number = new Date().getFullYear();
+  constructor() {}
+}
